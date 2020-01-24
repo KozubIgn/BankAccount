8d7abed7097fe9d7a0b523e3a3e3d334acdaa8b4
KozubIgn <ignacykozub@gmail.com>
2020-01-24 12:01:59 +0100

    added constructor

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 public class Main{
     public static void main(String [] args) {
     
-        BankAccount bobsAccount = new BankAccount();
+        BankAccount bobsAccount = new BankAccount("1233",0.00,"Bobo Brown","bob@gmail.com","4556545679");
         bobsAccount.setNumber("123456");
         bobsAccount.withdrawal(100.0);
         bobsAccount.deposit(50.0);
