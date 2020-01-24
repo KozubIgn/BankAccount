3c8220b79154d8a6cc4d2c3ff92e7c2c4d77c5db
KozubIgn <ignacykozub@gmail.com>
2020-01-24 14:18:23 +0100

    Added Vipcustomer.java

--- BankAccount.java
+++ BankAccount.java
@@ -3,17 +3,28 @@ public class BankAccount {
     private double balance;
     private String customerName;
     private String email;
-    private int phoneNumber;
+    private String phoneNumber;
+
+public BankAccount(){
+}
+//constructor
+public BankAccount(String number, double balance, String customerName, String email, String phoneNumber){
+this. number = number;
+this.balance = balance;
+this.customerName = customerName;
+this.email = email;
+this.phoneNumber = phoneNumber;
+    }
 
 public void deposit(double depositAmount){
     this.balance += depositAmount;
 }
 public void withdrawal(double withdrawalAmount){
     if(balance - withdrawalAmount < 0){
-        System.out.println("Only " + this.balance +" available.not processed" );
+        System.out.println("Only " + this.balance +" available. Not processed" );
     }else{
         this.balance -=withdrawalAmount;
-        System.out.println("Withdrawal of" + withdrawalAmount + "processed.Remaining balance = " + this.balance);
+        System.out.println("Withdrawal of " + withdrawalAmount + " processed. Remaining balance = " + this.balance);
     }
 }
 
@@ -44,8 +55,12 @@ public String getEmail(){
 public void setPhoneNumber(){
     this.phoneNumber = phoneNumber;
 }
-public int getPhoneNumber(){
+public String getPhoneNumber() {
     return this.phoneNumber;
 }
+//second constructor
+    public BankAccount(String customerName, String email, String phoneNumber) {
+        this("99999",200.55,customerName,email,phoneNumber);
+    }
 
 }
--- Main.java
+++ Main.java
@@ -2,9 +2,25 @@ public class Main{
     public static void main(String [] args) {
     
         BankAccount bobsAccount = new BankAccount("1233",0.00,"Bobo Brown","bob@gmail.com","4556545679");
-        bobsAccount.setNumber("123456");
-        bobsAccount.withdrawal(100.0);
-        bobsAccount.deposit(50.0);
-        bobsAccount.withdrawal(50.0);
+        //new client with second constructor
+        BankAccount timsAccount = new BankAccount("Tim","tim@gmai.com","3456754356");
+        System.out.println(timsAccount.getCustomerName()+ " name" + timsAccount.getAccountNumber() + " Number account");
+        System.out.println("current balance: " + timsAccount.getBalance());
+        timsAccount.withdrawal(10);
+
+        // Create a new class VipCustomer
+        // it should have 3 fields name, credit limit, and email address.
+        // create 3 constructors
+        // 1st constructor empty should call the constructor with 3 parameters with default values
+        // 2nd constructor should pass on the 2 values it receives and add a default value for the 3rd
+        // 3rd constructor should save all fields.
+        // create getters only for this using code generat
+
+        VipCustomer person1 = new VipCustomer();
+        System.out.println(person1.getName());
+        VipCustomer person2 = new VipCustomer("ted", 25000.0);
+        System.out.println(person2.getName());
+        VipCustomer person3 = new VipCustomer("tim",123.34,"tim@gmail.com");
+        System.out.println(person3.getEmail());
+} 
     }
-}
